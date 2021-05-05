aa0640ca045dff1768b8860464a3eda1601e2e3b
eltoto1219 <antonio36764@gmail.com>
2021-05-04 23:22:30 -0400

    testing

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,8 @@ const prod = {
 };
 const dev = {
  url: {
-  API_URL: 'http://localhost:5000'
+  //API_URL: 'http://localhost:5000',
+  API_URL: 'http://18.215.154.83',
  }
 };
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
